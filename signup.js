bd1735261eabdafa12e233c36a41c56afbc0090e
Nitro <the8bitnoob.gg@gmail.com>
2021-07-03 10:12:16 -0400

    Finish signup

--- signup.js
+++ signup.js
@@ -1,4 +1,4 @@
-let signup =  document.getElementById("form-signup")
+let signup = document.getElementById("form-signup")
 
 signup.onsubmit = (e) => {
     e.preventDefault();
@@ -7,9 +7,46 @@ signup.onsubmit = (e) => {
     let email = signup.email.value
     let password = signup.password.value
     let cfpass = signup.cfpass.value
-    
+
     console.log(name);
     console.log(email);
     console.log(password);
     console.log(cfpass);
+
+    let data = {
+        name: name,
+        email: email,
+        password: password
+    }
+
+    if (name) {} else {
+        setTextError("#name-err", "Field Required")
+    }
+
+    if (email) {} else {
+        setTextError("#email-err", "Field Required")
+    }
+
+    if (password) {
+        if (password.length > 6) {
+            if (password == cfpass) {
+                console.log(data)
+            } else {
+                console.log("aloooo")
+                setTextError("#cfpass-err", "Passwords Do Not Match")
+            }
+        } else {
+            setTextError("#password-err", "6 Required Characters")
+        }
+    } else {
+        setTextError("#password", "Field Required")
+    }
+
+    if (cfpass) {} else {
+        setTextError("#cfpass-err", "Field Required")
+    }
+}
+
+let setTextError = (tagname, content) => {
+    document.querySelector(tagname).innerHTML = content
 }
