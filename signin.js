6b6482a005ce139f8741c7005da4b04cfe0d372e
Nitro <the8bitnoob.gg@gmail.com>
2021-07-19 11:13:54 -0400

    Sync signin and signup with firebase

--- signin.js
+++ signin.js
@@ -1,60 +1,48 @@
-var actionCodeSettings = {
-    // URL you want to redirect back to. The domain (www.example.com) for this
-    // URL must be in the authorized domains list in the Firebase Console.
-    url: 'https://www.example.com/finishSignUp?cartId=1234',
-    // This must be true.
-    handleCodeInApp: true,
-    iOS: {
-        bundleId: 'com.example.ios'
-    },
-    android: {
-        packageName: 'com.example.android',
-        installApp: true,
-        minimumVersion: '12'
-    },
-    dynamicLinkDomain: 'example.page.link'
-};
+let form_signin = document.querySelector("#form_signin")
+form_signin.onsubmit = (e) => {
+    e.preventDefault()
 
-firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
-    .then(() => {
-        // The link was successfully sent. Inform the user.
-        // Save the email locally so you don't need to ask the user for it again
-        // if they open the link on the same device.
-        window.localStorage.setItem('emailForSignIn', email);
-        // ...
-    })
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-    });
+    let email = form_signin.email.value
+    let password = form_signin.password.value
 
-// Confirm the link is a sign-in with email link.
-if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
-    // Additional state parameters can also be passed via URL.
-    // This can be used to continue the user's intended action before triggering
-    // the sign-in operation.
-    // Get the email if available. This should be available if the user completes
-    // the flow on the same device where they started it.
-    var email = window.localStorage.getItem('emailForSignIn');
-    if (!email) {
-        // User opened the link on a different device. To prevent session fixation
-        // attacks, ask the user to provide the associated email again. For example:
-        email = window.prompt('Please provide your email for confirmation');
+    let data = {
+        email: email,
+        password: password
     }
-    // The client SDK will parse the code from the link for you.
-    firebase.auth().signInWithEmailLink(email, window.location.href)
-        .then((result) => {
-            // Clear email from storage.
-            window.localStorage.removeItem('emailForSignIn');
-            // You can access the new user via result.user
-            // Additional user info profile not available via:
-            // result.additionalUserInfo.profile == null
-            // You can check if the user is new or existing:
-            // result.additionalUserInfo.isNewUser
+
+    signIn_process(data)
+
+}
+
+let signIn_process = async (data) => {
+    let email = data.email
+    let password = data.password
+    try {
+        let result = await firebase.auth().signInWithEmailAndPassword(email, password)
+        let user = result.user
+        if (user.emailVerified) {
+            window.open("./chat.html")
+        } else {
+            throw new Error('Must verify email!')
+        }
+    } catch (error) {
+
+        let message = error.message
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
         })
-        .catch((error) => {
-            // Some error occurred, you can inspect the code: error.code
-            // Common errors could be invalid email and invalid or expired OTPs.
-        });
+
+        Toast.fire({
+            icon: 'error',
+            title: message
+        })
+    }
 }
--- signup.js
+++ signup.js
@@ -61,9 +61,39 @@ let signUp_process = async (data)=>{
             displayName: name
         })
         await firebase.auth().currentUser.sendEmailVerification()
-        alert("success")
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          
+          Toast.fire({
+            icon: 'success',
+            title: 'Signed up successfully'
+          })
     } catch (error) {
         let message = error.message
-        alert(message)
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          
+          Toast.fire({
+            icon: 'error',
+            title: message
+          })
     }
 }
